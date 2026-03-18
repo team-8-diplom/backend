63e17450dda112c643cfd310a14d1f6a06d38e4b
liaisan1 <lyasan04101996@gmail.com>
2026-03-18 20:41:43 +0300

    Revert "feat:add models and basic infrastructure (#14)"
    This reverts commit 4b031a1e0a9fcc41e49210992edaaad4bdb387f9.

--- app/models/base.py
+++
@@ -1,7 +0,0 @@
-from uuid import UUID, uuid4
-
-from sqlmodel import Field, SQLModel
-
-
-class Base(SQLModel):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
--- app/services/users.py
+++
@@ -1,8 +0,0 @@
-from app.dependencies.repositories import UserRepository, UserRepositoryDep
-
-
-class UserService:
-    __user_repository: UserRepository
-
-    def __init__(self, user_repository: UserRepositoryDep):
-        self.__user_repository = user_repository
