8d2a655a8909ca66b1c71e15771a7326b5c774db
liaisan1 <lyasan04101996@gmail.com>
2026-03-18 20:44:08 +0300

    Revert "feat:add models and basic infrastructure" (#15)

--- app/routers/departments.py
+++
@@ -1,9 +0,0 @@
-# departments.py
-from fastapi import APIRouter
-from app.dependencies.services import DepartmentServiceDep
-
-router = APIRouter(prefix="/departments", tags=["Departments"])
-
-@router.get("/")
-async def get_departments(service: DepartmentServiceDep):
-    ...
