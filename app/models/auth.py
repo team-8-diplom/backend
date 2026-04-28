0f11b1efcd1fe2f6bfc0c0afadbd7cfe04a50fc1
karim <karimiyarullin23@gmail.com>
2026-04-28 16:44:27 +0300

    fix(aurh): перенёс все схемы в отдельный файл models/auth

--- app/models/auth.py
+++ app/models/auth.py
@@ -1,6 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class AccessTokenResponse(BaseModel):
     access_token: str
     token_type: str = 'bearer'
+
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class TokenPairResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    refresh_token_max_age: int
