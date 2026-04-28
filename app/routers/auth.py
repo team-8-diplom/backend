2e146aaba22a6d39fbd01a99306084bff091950d
ainsual2 <yarullinkarim7@gmail.com>
2026-04-28 16:23:06 +0300

    Fix/task 1577173 (#42)

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -47,8 +47,11 @@ async def login(
     response: Response,
 ):
     """Логин и установка Refresh Token в httponly cookie."""
-    login_data = LoginRequest(email=form_data.username, password=form_data.password)
-    auth_result = await service.login(login_data, user_service, refresh_session_service)
+    auth_result = await service.login(
+        LoginRequest(email=form_data.username, password=form_data.password),
+        user_service,
+        refresh_session_service,
+    )
 
     response.set_cookie(
         key='refresh_token',
@@ -122,4 +125,4 @@ async def refresh_tokens(
         path='/',
     )
 
-    return RefreshTokenResponse(access_token=auth_result.access_token)
+    return AccessTokenResponse(access_token=auth_result.access_token)
